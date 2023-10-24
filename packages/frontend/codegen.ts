89b3fa3337fe6cf2f5c19b16df9396ae16aaec1f
maynguyen <nhkngan75@gmail.com>
2023-10-24 16:17:45 +1100

    feat: codegen setup and simple api call

---
+++ packages/frontend/codegen.ts
@@ -0,0 +1,15 @@
+import { CodegenConfig } from "@graphql-codegen/cli";
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "../../schema.graphql",
+  documents: ["src/**/*.tsx", "src/**/*.gql.ts"],
+  ignoreNoDocuments: true, // for better experience with the watcher
+  generates: {
+    "./src/generated/graphql.tsx": {
+      plugins: ["typescript", "typescript-operations", "typescript-react-apollo"],
+    },
+  },
+};
+
+export default config;
